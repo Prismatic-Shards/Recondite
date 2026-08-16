42461d380a52f34103cef3e14024aa2052d9799d
BananaKingXO <BananKingVr@gmail.com>
2026-08-15 22:53:10 -0500

    🐛 Fix Electrostatic Charge sound

--- bundle.sh
+++ bundle.sh
@@ -9,15 +9,15 @@ rm -rf artifacts/${version}
 mkdir -p artifacts/${version}
 
 cd datapack/
-zip -r ../artifacts/${version}/Spellbooks-${version}.zip data/ pack.mcmeta pack.png
-cp ../artifacts/${version}/Spellbooks-${version}.zip ../artifacts/${version}/mod.zip
+zip -r ../artifacts/${version}/Recondite-${version}.zip data/ pack.mcmeta pack.png
+cp ../artifacts/${version}/Recondite-${version}.zip ../artifacts/${version}/mod.zip
 
 cd ../resource_pack
-zip -r ../artifacts/${version}/Spellbooks-${version}-RP.zip assets/ pack.mcmeta pack.png
+zip -r ../artifacts/${version}/Recondite-${version}-RP.zip assets/ pack.mcmeta pack.png
 zip -r ../artifacts/${version}/mod.zip assets/ pack.png pack.mcmeta
 cd ../mod
 zip -r ../artifacts/${version}/mod.zip *
 cd ../artifacts/${version}
-mv mod.zip Spellbooks-${version}.jar
+mv mod.zip Recondite-${version}.jar
 
 cd ..
