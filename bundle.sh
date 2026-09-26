68304186cf85378dbd4cfa0254b3c10c8c4f4bb9
BananaKingXO <BananKingVr@gmail.com>
2026-09-26 10:29:10 -0500

    ✨Fixed stuff

--- bundle.sh
+++ bundle.sh
@@ -14,7 +14,7 @@ cp ../artifacts/${version}/Recondite-${version}.zip ../artifacts/${version}/mod.
 
 cd ../resource_pack
 zip -r ../artifacts/${version}/Recondite-${version}-RP.zip assets/ pack.mcmeta pack.png
-zip -r ../artifacts/${version}/mod.zip assets/ pack.png pack.mcmeta
+zip -r ../artifacts/${version}/mod.zip assets/
 cd ../mod
 zip -r ../artifacts/${version}/mod.zip *
 cd ../artifacts/${version}
